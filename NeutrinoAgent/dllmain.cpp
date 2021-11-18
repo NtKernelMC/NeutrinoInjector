634466ea8ff9221af9c1c023fac9e3539bcde14a
NtKernelMC <hadeweek@gmail.com>
2021-11-18 06:56:14 +0100

    Initial Commit

---
+++ NeutrinoAgent/dllmain.cpp
@@ -0,0 +1,130 @@
+﻿// Version: 0.0.1 ALFA
+#ifndef _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+#include <Windows.h>
+#include <stdio.h>
+#include "..\Registry.h"
+#include "..\MMAP.h"
+#include <string>
+#include <winternl.h>
+#include "MinHook.h"
+#include <Psapi.h>
+#pragma comment(lib, "libMinHook.x86.lib")
+bool InjectDLL(HANDLE hProc, const std::string& DLL_Path)
+{
+	if (hProc == nullptr || DLL_Path.empty()) return false;
+	long dll_size = DLL_Path.length() + 1;
+	LPVOID MyAlloc = VirtualAllocEx(hProc, NULL, dll_size, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+	if (MyAlloc == NULL) return false;
+	int IsWriteOK = WriteProcessMemory(hProc, MyAlloc, DLL_Path.c_str(), dll_size, 0);
+	if (IsWriteOK == 0) return false;
+	DWORD dWord = NULL; 
+	#pragma warning(suppress: 6387)
+	LPTHREAD_START_ROUTINE addrLoadLibrary = (LPTHREAD_START_ROUTINE)GetProcAddress(LoadLibraryA("kernel32.dll"), "LoadLibraryA");
+	HANDLE ThreadReturn = CreateRemoteThread(hProc, NULL, 0, addrLoadLibrary, MyAlloc, 0, &dWord);
+	if (ThreadReturn == NULL) return false;
+	if ((hProc != NULL) && (MyAlloc != NULL) && (IsWriteOK != ERROR_INVALID_HANDLE) && (ThreadReturn != NULL)) return true;
+	return false;
+}
+void ParseAndLoad(HANDLE hProc)
+{
+	CEasyRegistry* reg = new CEasyRegistry(HKEY_CURRENT_USER, "Software\\Neutrino", false);
+	if (reg)
+	{
+		std::string homePath = reg->ReadString("HomeDir");
+		if (!homePath.empty())
+		{
+			WIN32_FIND_DATAA FindFileData{ 0 };
+			HANDLE hFind = FindFirstFileExA((homePath + "\\*.dll").c_str(),
+			FindExInfoStandard, &FindFileData, FindExSearchNameMatch, NULL, 0);
+			if (hFind != INVALID_HANDLE_VALUE)
+			{
+				do
+				{
+					std::string full_name = homePath + "\\" + FindFileData.cFileName;
+					if (full_name.find("NeutrinoAgent") == std::string::npos) InjectDLL(hProc, full_name);
+				} while (FindNextFileA(hFind, &FindFileData));
+				FindClose(hFind);
+			}
+		}
+		delete reg;
+	}
+}
+typedef HANDLE (__stdcall *ptrCreateRemoteThreadEx)(HANDLE hProcess, LPSECURITY_ATTRIBUTES lpThreadAttributes, SIZE_T dwStackSize,
+LPTHREAD_START_ROUTINE lpStartAddress, LPVOID lpParameter, DWORD dwCreationFlags, 
+LPPROC_THREAD_ATTRIBUTE_LIST lpAttributeList, LPDWORD lpThreadId);
+ptrCreateRemoteThreadEx callCreateRemoteThreadEx = nullptr;
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+HANDLE __stdcall hookedCreateRemoteThreadEx(HANDLE hProcess, LPSECURITY_ATTRIBUTES lpThreadAttributes, SIZE_T dwStackSize,
+LPTHREAD_START_ROUTINE lpStartAddress, LPVOID lpParameter, DWORD dwCreationFlags,
+LPPROC_THREAD_ATTRIBUTE_LIST lpAttributeList, LPDWORD lpThreadId)
+{
+	HANDLE hndl = callCreateRemoteThreadEx(hProcess, lpThreadAttributes, dwStackSize,
+	lpStartAddress, lpParameter, dwCreationFlags, lpAttributeList, lpThreadId);
+	char procPath[256]; memset(procPath, 0, sizeof(procPath));
+	GetProcessImageFileNameA(hProcess, procPath, sizeof(procPath));
+	if (strstr(procPath, "gta_sa") != nullptr || strstr(procPath, "proxy_sa") != nullptr)
+	{
+		static bool only_once = true;
+		if (only_once)
+		{
+			only_once = false;
+			FILE* hFile = fopen("C:\\Users\\hadew\\Desktop\\Neutrino\\logger.txt", "a+");
+			if (hFile)
+			{
+				fprintf(hFile, "Path: %s\n", procPath);
+				fclose(hFile);
+			}
+			ParseAndLoad(hProcess);
+			#pragma warning(suppress: 26812)
+			MH_DisableHook(MH_ALL_HOOKS);
+		}
+	}
+	return hndl;
+}
+void LoadHacks()
+{
+	#pragma warning(suppress: 6387)
+	callCreateRemoteThreadEx = (ptrCreateRemoteThreadEx)GetProcAddress(GetModuleHandleA("kernelbase.dll"), "CreateRemoteThreadEx");
+	#pragma warning(suppress: 26812)
+	MH_Initialize(); 
+	MH_CreateHook(callCreateRemoteThreadEx, &hookedCreateRemoteThreadEx, reinterpret_cast<LPVOID*>(&callCreateRemoteThreadEx));
+	MH_EnableHook(MH_ALL_HOOKS);
+	CEasyRegistry* reg = new CEasyRegistry(HKEY_CURRENT_USER, "Software\\Neutrino", false);
+	if (reg)
+	{
+		DWORD StoredCode = reg->ReadInteger("StoredData");
+		DWORD SpinLockAddr = reg->ReadInteger("SpinLock");
+		BYTE oldCode[5] = { 0x0, 0x0, 0x0, 0x0, 0x0 };
+		memcpy(oldCode, (PVOID)StoredCode, 5);
+		DWORD oldProt = 0x0; VirtualProtect((PVOID)SpinLockAddr, 5, PAGE_EXECUTE_READWRITE, &oldProt);
+		memcpy((PVOID)SpinLockAddr, oldCode, 5);
+		VirtualProtect((PVOID)SpinLockAddr, 5, oldProt, &oldProt);
+		DWORD thID = reg->ReadInteger("Thread");
+		HANDLE hThread = OpenThread(THREAD_ALL_ACCESS, FALSE, thID);
+		if (hThread)
+		{
+			ResumeThread(hThread);
+			CloseHandle(hThread);
+		}
+		delete reg;
+	}
+}
+int __stdcall DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+        LoadHacks();
+        break;
+    case DLL_THREAD_ATTACH:
+        break;
+    case DLL_THREAD_DETACH:
+        break;
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return 1;
+}
+
---
+++ Source.cpp
@@ -0,0 +1,109 @@
+// Version: 0.0.1 ALFA
+#ifndef _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+#include <Windows.h>
+#include <stdio.h>
+#include <string>
+#include <tchar.h>
+#include <direct.h>
+#include "Registry.h"
+#include <TlHelp32.h>
+#include "MMAP.h"
+int main()
+{
+	system("color 5F"); 
+	SetConsoleTitleA("Neutrino Injector by NtKernelMC | MFT 2021");
+	printf("[INFO] Searching for MTA Province...\n");
+	CEasyRegistry* reg = new CEasyRegistry(HKEY_LOCAL_MACHINE, "Software\\WOW6432Node\\Multi Theft Auto: Province All\\Common", false);
+	if (reg)
+	{
+		std::string prov_path = reg->ReadString("GTA:SA Path");
+		if (prov_path.empty())
+		{
+			printf("[ERROR] Can`t find installed game!\n");
+			return 0;
+		}
+		std::string tmpDir = prov_path;
+		prov_path += "\\MTA\\Multi Theft Auto.exe"; delete reg;
+		printf("[INFO] Found: %s\n[INFO] Starting Multi Theft Auto.exe...\n", prov_path.c_str());
+		STARTUPINFOA info = { sizeof(info) }; PROCESS_INFORMATION processInfo;
+		static char tmpS[256]; memset(tmpS, 0, sizeof(tmpS));
+		sprintf(tmpS, "\"%s\" upd", prov_path.c_str());
+		BOOL rslt = CreateProcessA(NULL, tmpS, NULL, NULL, TRUE, CREATE_SUSPENDED, NULL, tmpDir.c_str(), &info, &processInfo);
+		if (rslt)
+		{
+			CONTEXT ctx{ 0 }; ctx.ContextFlags = CONTEXT_ALL; 
+			#pragma warning(suppress: 6387)
+			rslt = GetThreadContext(processInfo.hThread, &ctx);
+			if (rslt)
+			{
+				printf("[DBG] Saving entry point instruction...\n"); DWORD dummy = NULL;
+				PVOID oldCodeMem = VirtualAllocEx(processInfo.hProcess, 0, 5, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+				if (!oldCodeMem)
+				{
+					printf("\nError: Unable to allocate memory for DLL data (Error: %d)\n", GetLastError());
+					Sleep(3000); ExitProcess(0);
+				}
+				else printf("[MEMORY] Allocated data region at address: 0x%X\n", (DWORD)oldCodeMem);
+				BYTE oldCode[5] = { 0x0, 0x0, 0x0, 0x0, 0x0 };
+				#pragma warning(suppress: 6387)
+				rslt = ReadProcessMemory(processInfo.hProcess, (PVOID)ctx.Eax, oldCode, 5, &dummy);
+				if (!rslt)
+				{
+					printf("\nError: Unable to read memory! Last Error Code: (%d)\n", GetLastError());
+					Sleep(3000); ExitProcess(0);
+				}
+				rslt = WriteProcessMemory(processInfo.hProcess, oldCodeMem, oldCode, 5, &dummy);
+				if (rslt)
+				{
+					CEasyRegistry* n_reg = new CEasyRegistry(HKEY_CURRENT_USER, "Software\\Neutrino", true);
+					if (n_reg)
+					{
+						n_reg->WriteInteger("StoredData", (DWORD)oldCodeMem);
+						n_reg->WriteInteger("SpinLock", ctx.Eax);
+						n_reg->WriteInteger("Thread", processInfo.dwThreadId);
+						printf("[DBG] Instruction writed to allocated memory!\n");
+						PVOID spinlock_addr = (PVOID)ctx.Eax; BYTE SpinLockCode[5] = { 0xE9, 0x90, 0x90, 0x90, 0x90 };
+						#pragma warning(suppress: 4477)
+						#pragma warning(suppress: 6273)
+						printf("[DBG] Address of OEP for spinlock: 0x%X\n", spinlock_addr);
+						DWORD Delta = (DWORD)spinlock_addr - (DWORD)spinlock_addr - 5;
+						memcpy(&SpinLockCode[1], &Delta, 4); DWORD oldProt = 0x0;
+						rslt = VirtualProtectEx(processInfo.hProcess, spinlock_addr, 5, PAGE_EXECUTE_READWRITE, &oldProt);
+						if (!rslt)
+						{
+							printf("\nError: Unable to change memory access! Last Error Code: (%d)\n", GetLastError());
+							Sleep(3000); ExitProcess(0);
+						}
+						printf("[DBG] Writing spinlock shellcode to process...\n");
+						#pragma warning(suppress: 6387)
+						rslt = WriteProcessMemory(processInfo.hProcess, spinlock_addr, SpinLockCode, 5, &dummy);
+						if (rslt)
+						{
+							printf("[SUCCESS] Spinlock shellcode successfully placed!\n");
+							char homePath[256]; memset(homePath, 0, sizeof(homePath));
+							#pragma warning(suppress: 6031)
+							_getcwd(homePath, 256); n_reg->WriteString("HomeDir", homePath); 
+							rslt = VirtualProtectEx(processInfo.hProcess, spinlock_addr, 5, oldProt, &oldProt);
+							if (!rslt)
+							{
+								printf("\nError: Unable to change memory access! Last Error Code: (%d)\n", GetLastError());
+								Sleep(3000); ExitProcess(0);
+							}
+							MmapDLL(processInfo.hProcess, "NeutrinoAgent.dll"); delete n_reg;
+							Sleep(3000); ExitProcess(0);
+						}
+						else printf("[ERROR] #2 Can`t write to process :( Last Error Code: %d\n", GetLastError());
+					}
+				}
+				else printf("[ERROR] #1 Can`t write to process :( Last Error Code: %d\n", GetLastError());
+			}
+			else printf("[ERROR] Can`t obtain thread context. Last Error Code: %d\n", GetLastError());
+			CloseHandle(processInfo.hThread); CloseHandle(processInfo.hProcess);
+		}
+		else printf("[ERROR] Cannot create process :( Last Error Code: %d\n", GetLastError());
+	}
+	while (true) { Sleep(100); }
+	return 1;
+}
